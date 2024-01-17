a2a14f94e30be048aee9e0e547081a13f9415924
Temmiie <mattheotermine104@gmail.com>
2024-01-17 22:51:52 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import react from '@vitejs/plugin-react';
 
 export default () => {
     return defineConfig({
-        plugins: [vue()],
-
+        plugins: [vue(), react()],
         server: {
             host: true,
             port: 3000
